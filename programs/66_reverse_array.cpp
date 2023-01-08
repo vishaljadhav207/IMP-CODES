3333ab8c8fd2bd1b911204d1f83473369b73183f
vishal jadhav <vj7708152@gmail.com>
2023-01-08 09:42:29 +0530

    #66

---
+++ programs/66_reverse_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << " " << arr[i];
+    }
+    return 0;
+}
